d8e245e48c92cc60be7b8160dab46fb24a829ea0
John Kingoo <kingoojohn27@gmail.com>
2026-07-21 14:48:17 +0300

    Create course model

--- students/models.py
+++ students/models.py
@@ -15,11 +15,19 @@ class Student(models.Model):#This creates a new model called Student, which will
     def __str__(self):
         return self.user.get_full_name() or self.user.username   #This method defines how the Student object will be represented as a string. It returns the full name of the associated user if available; otherwise, it returns the username.
     
-    
+
 class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    employee_number = models.CharField(max_length=20, unique=True)
-    department = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)#This means that a user can only have one teacher profile...
+    employee_number = models.CharField(max_length=20, unique=True)#CharField is used to store text data, and unique=True ensures that each employee number is unique across all teacher records.
+    department = models.CharField(max_length=30)#CharField is used to store text data,department where the teacher belongs, and max_length=30 sets the maximum length for the department field.
 
     def __str__(self):
-        return self.user.get_full_name() or self.user.username
+        return self.user.get_full_name() or self.user.username   #This method defines how the Teacher object will be represented as a string. It returns the full name of the associated user if available; otherwise, it returns the username.
+    
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)#CharField is used to store text data, and max_length=100 sets the maximum length for the course name.
+    course_code = models.CharField(max_length=10, unique=True)#CharField is used to store text data, and unique=True ensures that each course code is unique across all course records.
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name="courses")#This creates a foreign key relationship to the Teacher model, indicating that each course is associated with a specific teacher.on_delete=models.CASCADE means that if a teacher is deleted, all their associated courses will also be deleted. related_name="courses" allows you to access the courses taught by a teacher using teacher.courses.   
+def __str__(self):
+        return f"{self.course_name} ({self.course_code})"   #This method defines how the Course object will be represented as a string. It returns the course name along with its code in parentheses.
