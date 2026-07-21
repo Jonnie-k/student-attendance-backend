aa1c3bb17b031d290e751e77145cea02b9560233
John Kingoo <kingoojohn27@gmail.com>
2026-07-21 15:45:36 +0300

    Register student model

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("admission_number", "user", "phone_number", "gender")
+    search_fields = ("admission_number", "user__username", "user__first_name")
+    list_filter = ("gender",)
