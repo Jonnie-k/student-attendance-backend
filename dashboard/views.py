b230502d84ef7d6b5b5ca17a7004c9bc8ef20201
John Kingoo <kingoojohn27@gmail.com>
2026-07-22 22:23:35 +0300

    Create role-based dashboars

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, "home/index.html")
+
+
+@login_required
+def dashboard(request):
+    if request.user.is_superuser:
+        return render(request, "dashboard/admin_dashboard.html")
+
+    if hasattr(request.user, "teacher"):
+        return render(request, "dashboard/teacher_dashboard.html")
+
+    if hasattr(request.user, "student"):
+        return render(request, "dashboard/student_dashboard.html")
+
+    return render(request, "dashboard/admin_dashboard.html")
