0c80d18fc3e5c456c9403f1a8d1446ca25caca82
John Kingoo <kingoojohn27@gmail.com>
2026-07-23 13:15:29 +0300

    Improve student serializer response

--- students/serializers.py
+++ students/serializers.py
@@ -3,6 +3,17 @@ from .models import Student
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source="user.get_full_name", read_only=True)
+    username = serializers.CharField(source="user.username", read_only=True)
+
     class Meta:
         model = Student
-        fields = "__all__"
+        fields = [
+            "id",
+            "user",
+            "username",
+            "full_name",
+            "admission_number",
+            "phone_number",
+            "gender",
+        ]
