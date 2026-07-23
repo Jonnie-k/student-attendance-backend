750570f6d25b230926aa84d3fb67d95fc07079b3
John Kingoo <kingoojohn27@gmail.com>
2026-07-23 13:30:52 +0300

    Add search, filtering and ordering to student API

--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from .models import Student
 from .serializers import StudentSerializer
 
@@ -7,3 +9,24 @@ from .serializers import StudentSerializer
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+
+    filterset_fields = [
+        "gender",
+    ]
+
+    search_fields = [
+        "admission_number",
+        "user__username",
+        "user__first_name",
+        "user__last_name",
+    ]
+
+    ordering_fields = [
+        "admission_number",
+    ]
